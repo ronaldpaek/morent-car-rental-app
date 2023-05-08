ac927506fc69f341af917c31f7997ba659cae06c
ermz <eramirez4369@gmail.com>
2023-05-08 01:43:44 -0400

    Added search and fixed TS issues

---
+++ app/api/cars/search/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from 'lib/prisma';
+
+export async function GET(req: Request) {
+    const { searchParams } = req.nextUrl
+    try {
+        const make = searchParams.get('make')
+        const seatCapacity = parseInt(searchParams.get('seatCapacity'))
+        const type = searchParams.get('type')
+        const location = searchParams.get('location')
+        const price = parseFloat(searchParams.get('price'))
+        const cars = await prisma.car.findMany({
+            where: {
+               make: {
+                   contains: make || undefined
+               },
+               seatCapacity: seatCapacity || undefined,
+               bodyType: type || undefined,
+               location: location || undefined,
+               rentPrice: {
+                   lt: price || undefined
+               }
+            }
+        });
+
+        return new NextResponse(JSON.stringify(cars), { status: 200 });
+    } catch (err: any) {
+        return new NextResponse(JSON.stringify({ error: err.message }), { status: 500 });
+    }
+
+}
